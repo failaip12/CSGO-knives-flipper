a80fbca18f4775388c0a79ca9621becf06e9e8be
failaip12 <86018517+failaip12@users.noreply.github.com>
2025-02-12 22:24:45 +0100

    fix

--- backend/scraper/Knife.py
+++ backend/scraper/Knife.py
@@ -11,6 +11,7 @@ class Knife:
         last_min_price_with_fee: Optional[float],
         last_min_price_without_fee: Optional[float],
         buy_order_price: Optional[float],
+        knife_image: Optional[str] = None,
         last_sold: Optional[datetime] = None
     ):
         self.knife_name = knife_name
@@ -22,6 +23,7 @@ class Knife:
         self.buy_order_price = buy_order_price
         self.last_updated = datetime.now()
         self.last_sold = last_sold
+        self.knife_image = knife_image
 
     # For debugging or detailed inspection (called in interactive environments)
     def __repr__(self):
@@ -31,7 +33,8 @@ class Knife:
                 f"last_min_price_with_fee={self.last_min_price_with_fee}, "
                 f"last_min_price_without_fee={self.last_min_price_without_fee}, "
                 f"buy_order_price={self.buy_order_price}, "
-                f"last_updated={self.last_updated}, last_sold={self.last_sold})")
+                f"last_updated={self.last_updated}, last_sold={self.last_sold})"
+                f"knife_image={self.knife_image}")
 
     # For a user-friendly print output (called by print())
     def __str__(self):
@@ -43,4 +46,5 @@ class Knife:
                 f"Last Min Price (Without Fee): {self.last_min_price_without_fee}\n"
                 f"Buy Order Price: {self.buy_order_price}\n"
                 f"Last Updated: {self.last_updated}\n"
-                f"Last Sold: {self.last_sold}")
+                f"Last Sold: {self.last_sold}\n"
+                f"Knife Image: {self.knife_image}")
